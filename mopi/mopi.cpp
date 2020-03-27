df93bdf9bb4d79cb4f5a1bd00b33a45cfe323673
严浩 <1424412158@qq.com>
2020-03-27 15:32:26 +0800

    1

--- mopi/mopi.cpp
+++ mopi/mopi.cpp
@@ -1,20 +1,37 @@
-﻿// mopi.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
+﻿#include <iostream>
+#include <opencv2/opencv.hpp>
+using namespace cv;
+using namespace std;
+int main() {
+	VideoCapture cap(0);
+	double scale = 0.5;
+	double i_minH = 0;
+	double i_maxH = 20;
+	double i_minS = 50;
+	double i_maxS = 255;
+	double i_minV = 50;
+	double i_maxV = 255;
+		Mat frame;
+		Mat hsvMat;
+		Mat detectMat;
+		Mat medMat;
+		Mat dipMat;
+	while (1)
+	{
+		cap >> frame;
+		Size ResImgsiz = Size(frame.cols*scale, frame.rows*scale);
+		Mat rFrame = Mat(ResImgsiz, frame.type());
+		resize(frame, rFrame, ResImgsiz, INTER_LINEAR);
+		cvtColor(rFrame, hsvMat, COLOR_BGR2HSV);
+		rFrame.copyTo(detectMat);
+		cv::inRange(hsvMat, Scalar(i_minH, i_minS, i_minV), Scalar(i_maxH, i_maxS, i_maxV), detectMat);
+		GaussianBlur(rFrame, medMat, Size(3, 3), 3, 3);
+		rFrame.copyTo(dipMat);
+		medMat.copyTo(dipMat, detectMat);
+		imshow("whie: in the range", detectMat);
+		imshow("dipMat", dipMat);
+		imshow("frame", rFrame);
+		waitKey(30);
 
-#include <iostream>
-
-int main()
-{
-    std::cout << "Hello World!\n";
+	}
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
